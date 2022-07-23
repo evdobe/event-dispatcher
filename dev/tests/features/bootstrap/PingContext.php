702e92756283e512eb4dff33ede469122a7c942e
Konstantinos Togias <info@ktogias.gr>
2022-07-23 21:06:37 +0300

    Fixed ping context for test env

--- dev/tests/features/bootstrap/PingContext.php
+++ dev/tests/features/bootstrap/PingContext.php
@@ -15,6 +15,8 @@ class PingContext implements Context
 {
     protected int $port;
 
+    protected string $host;
+
     protected Curl $curl;
 
     /**
@@ -34,6 +36,7 @@ class PingContext implements Context
     public function theServicePortIsDefined()
     {
         $this->port = getenv('HTTP_PORT');
+        $this->host = getenv('HTTP_HOST')?:'localhost';
     }
 
     /**
@@ -42,7 +45,7 @@ class PingContext implements Context
     public function iDoHttpGet()
     {
         $this->curl = new Curl();
-        $this->curl->get('http://localhost:'.$this->port);
+        $this->curl->get('http://'.$this->host.':'.$this->port);
     }
 
     /**
