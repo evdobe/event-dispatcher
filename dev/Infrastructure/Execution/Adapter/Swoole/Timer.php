23d9d88af4b95f00829ff00f0e28f52f6228a40d
XhmikosR <xhmikosr@gmail.com>
2022-08-26 15:55:59 +0300

    Add missing strict_types

--- dev/Infrastructure/Execution/Adapter/Swoole/Timer.php
+++ dev/Infrastructure/Execution/Adapter/Swoole/Timer.php
@@ -1,4 +1,4 @@
-<?php
+<?php declare(strict_types=1);
 
 namespace Infrastructure\Execution\Adapter\Swoole;
 
--- dev/tests/features/bootstrap/DispatchEventContext.php
+++ dev/tests/features/bootstrap/DispatchEventContext.php
@@ -1,4 +1,4 @@
-<?php
+<?php declare(strict_types=1);
 
 use Behat\Behat\Context\Context;
 use Behat\Gherkin\Node\PyStringNode;
